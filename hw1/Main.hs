eed848a8db2af5fd338375b3d7f1d5a2fdae741c
Oleg Stotsky <ostotsky@gmail.com>
2020-02-15 20:14:09 +0300

    hw1: add substitution

--- hw1/Main.hs
+++ hw1/Main.hs
@@ -12,6 +12,12 @@ shift val t = go 0 val t where
     go cutOff val (t1 :@: t2) = (go cutOff val t1) :@: (go cutOff val t2)
     go cutOff val (Lmb t) = Lmb $ go (cutOff + 1) val t
 
+substDB :: Int -> Term -> Term -> Term
+substDB j n (Idx x) | x == j = n
+substDB j n i@(Idx x) = i
+substDB j n (t1 :@: t2) = (substDB j n t1) :@: (substDB j n t2)
+substDB j n (Lmb t) = Lmb $ substDB (j+1) (shift 1 n) t
+
 main :: IO ()
 main = do
   let t1 = Lmb $ (Idx 0) :@: (Idx 1 :@: Idx 2)
